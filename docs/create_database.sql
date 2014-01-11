702971ed4c8aefab7b0b01215d6fbaf233ce80a6
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-01-11 10:50:07 +0200

    Database creation script

---
+++ docs/create_database.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE bills (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    hash VARCHAR(100) NOT NULL,
+    sender_iban VARCHAR(100) NOT NULL,
+    sender_swift VARCHAR(100) NOT NULL,
+    sender_name VARCHAR(100) NOT NULL,
+    sender_email VARCHAR(100),
+    sender_www VARCHAR(100),
+    sender_address VARCHAR(100),
+    sender_zip VARCHAR(10),
+    sender_city VARCHAR(100),
+    sender_yt VARCHAR(10),
+    payer_name VARCHAR(100) NOT NULL,
+    payer_address VARCHAR(100),
+    payer_zip VARCHAR(10),
+    payer_city VARCHAR(100),
+    bill_description VARCHAR(100) NOT NULL,
+    bill_duedate DATETIME NOT NULL,
+    bill_total FLOAT NOT NULL,
+    bill_number VARCHAR(100) NOT NULL,
+    bill_reference VARCHAR(100) NOT NULL
+);

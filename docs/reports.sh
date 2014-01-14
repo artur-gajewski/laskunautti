6c13385715a8f4d19fb761e2c5762f4fb12a8d40
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-01-14 08:58:51 +0200

    Added homepage loads

--- docs/reports.sh
+++ docs/reports.sh
@@ -10,20 +10,24 @@ echo "# Laskunautti activity report for today ($now)"
 echo "#"
 echo ""
 
+echo "Homepage loads today:"
+grep 'GET / \[\]' $logfile | wc -l
+echo ""
+
 echo "Sample viewed today:"
-grep 'Sample loaded' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Sample loaded' $logfile | wc -l
 echo ""
 
 echo "Previews created today:"
-grep 'Preview loaded' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Preview loaded' $logfile | wc -l
 echo ""
 
 echo "Invoices created today:"
-grep 'Invoice saved' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Invoice saved' $logfile | wc -l
 echo ""
 
 echo "Invoices viewed today:"
-grep 'Invoice viewed' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Invoice viewed' $logfile | wc -l
 echo ""
 
 echo "List of Invoices created today:"
@@ -40,20 +44,24 @@ echo "# Laskunautti activity report for yesterday ($yesterday)"
 echo "#"
 echo ""
 
+echo "Homepage loads yesterday:"
+grep 'GET / \[\]' $logfile | wc -l
+echo ""
+
 echo "Sample viewed yesterday:"
-grep 'Sample loaded' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Sample loaded' $logfile | wc -l
 echo ""
 
 echo "Previews created yesterday:"
-grep 'Preview loaded' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Preview loaded' $logfile | wc -l
 echo ""
 
 echo "Invoices created yesterday:"
-grep 'Invoice saved' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Invoice saved' $logfile | wc -l
 echo ""
 
 echo "Invoices viewed yesterday:"
-grep 'Invoice viewed' $logfile | sed 's/^.*INFO://' | wc -l
+grep 'Invoice viewed' $logfile | wc -l
 echo ""
 
 echo "List of Invoices created yesterday:"
